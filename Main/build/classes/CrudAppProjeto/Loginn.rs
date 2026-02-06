07a99233df5075eacbc6bcfd5cb9424a0f8821c4
João Pedro Antunes De Souza <teddbrgamer77@gmail.com>
2026-02-06 20:54:54 -0300

    Add files via upload

---
+++ Main/build/classes/CrudAppProjeto/Loginn.rs
@@ -0,0 +1,4 @@
+login.Loginn$3
+login.Loginn$2
+login.Loginn$1
+login.Loginn
---
+++ Main/src/CrudAppProjeto/Main.java
@@ -0,0 +1,10 @@
+
+package CrudAppProjeto;
+
+public class Main {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
